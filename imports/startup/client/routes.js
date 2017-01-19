7d67b79ef0b62f12f7b5128619eaa0b3eaa09380
Miguel Gimenez <gimenezgimenezmiguel@gmail.com>
2017-01-19 17:29:40 +0100

    test

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -4,7 +4,8 @@ import { Router, Route, browserHistory } from 'react-router';
 // route components
 // import AppContainer from '../../ui/containers/AppContainer.js';
 // import ListPageContainer from '../../ui/containers/ListPageContainer.js';
-gd// import AuthPageJoin from '../../ui/pages/AuthPageJoin.js';
+// import AuthPageSignIn from '../../ui/pages/AuthPageSignIn.js';
+// import AuthPageJoin from '../../ui/pages/AuthPageJoin.js';
 // import NotFoundPage from '../../ui/pages/NotFoundPage.js';
 import App from '../../ui/App.jsx';
 import Lobby from '../../ui/Lobby.jsx';
@@ -13,10 +14,7 @@ export const renderRoutes = () => (
   <Router history={browserHistory}>
     <Route path="/" component={App}>
       <Route path="/lobby" component={Lobby}/>
-
-
-
-
+      <Route path="/player" component={Player}/>
     </Route>
   </Router>
 );
