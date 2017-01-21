add95a65e86871ba6b538f6eb0d0a3d41e6d08b5
johnandblue <jc@johnandblue.com>
2017-01-21 17:36:59 +0100

    Added feat: pregame + rooms

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -8,6 +8,7 @@ injectTapEventPlugin();
 
 import App from '../../ui/App.jsx';
 import Lobby from '../../ui/Lobby.jsx';
+import PreGame from '../../ui/PreGame.jsx';
 import Root from '../../ui/Root.jsx';
 
 export const renderRoutes = () => (
@@ -16,7 +17,7 @@ export const renderRoutes = () => (
       <Route path="/" component={App}>
         <IndexRoute component={Root}/>
         <Route path="/game/:gameCode" component={Lobby} />
-        <Route path="/game/:gameCode/admin" component={Lobby} />
+        <Route path="/pregame/:gameCode" component={PreGame} />
       </Route>
     </Router>
   </MuiThemeProvider>
