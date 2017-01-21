3962702c4b12d84add85acaf1426a693b2eacaf1
johnandblue <jc@johnandblue.com>
2017-01-21 10:56:31 +0100

    added home button

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import {browserHistory } from 'react-router';
+import { Link } from 'react-router';
 import AppBar from 'material-ui/AppBar';
-
+import IconButton from 'material-ui/IconButton';
+import ActionHome from 'material-ui/svg-icons/action/home';
 
 const style={
   backgroundColor:'#D32F2F'
@@ -16,7 +18,11 @@ export default class App extends Component {
         <header>
           <AppBar
             title="Two Rooms and a Boom"
-            style={style}/>
+            style={style}
+            iconElementLeft={<IconButton
+            containerElement={<Link to="/" />}
+            ><ActionHome /></IconButton>}
+          />
         </header>
         {this.props.children}
       </div>
--- imports/ui/Lobby.jsx
+++ imports/ui/Lobby.jsx
@@ -20,108 +20,107 @@ const iconButtonElement = (
     touch={true}
     tooltip="more"
     tooltipPosition="bottom-left"
-  >
-    <MoreVertIcon color={grey400} />
-  </IconButton>
-);
+    >
+      <MoreVertIcon color={grey400} />
+    </IconButton>
+  );
 
-const rightIconMenu = (
-  <IconMenu iconButtonElement={iconButtonElement}>
-    <MenuItem>Reply</MenuItem>
-    <MenuItem>Forward</MenuItem>
-    <MenuItem>Delete</MenuItem>
-  </IconMenu>
-);
+  const rightIconMenu = (
+    <IconMenu iconButtonElement={iconButtonElement}>
+      <MenuItem>Change Game</MenuItem>
+      <MenuItem>Exit Game</MenuItem>
+    </IconMenu>
+  );
 
-const containerStyle = {
- margin: '0 auto',
- width: '100%',
- // padding: 20,
- display: 'flex'
-}
+  const containerStyle = {
+    margin: '0 auto',
+    width: '100%',
+    // padding: 20,
+    display: 'flex'
+  }
 
-const CardStyle={
- padding: 40,
- fontSize: 20,
- margin: 'auto',
- textAlign: 'center',
+  const CardStyle={
+    padding: 40,
+    fontSize: 20,
+    margin: 'auto',
+    textAlign: 'center',
 
-}
+  }
 
-const style = {
- margin:15,
-};
+  const style = {
+    margin:15,
+  };
 
 
-class Lobby extends React.Component {
+  class Lobby extends React.Component {
 
- // constructor(){
- //   super();
- //   this.state={
- //     code: {this.props.params.gameCode},
- //     players: []
- //   };
- // }
- //
- // addPlayer() {
- //   this.
- // }
+    // constructor(){
+    //   super();
+    //   this.state={
+    //     code: {this.props.params.gameCode},
+    //     players: []
+    //   };
+    // }
+    //
+    // addPlayer() {
+    //   this.
+    // }
 
- renderPlayers() {
-   let games = this.props.games;
+    renderPlayers() {
+      let games = this.props.games;
 
-   const currentGame = games.filter(game =>{
-     console.log(this.props.games);
-     return (this.props.params.gameCode===game.gameCode);
-   });
+      const currentGame = games.filter(game =>{
+        console.log(this.props.games);
+        return (this.props.params.gameCode===game.gameCode);
+      });
 
-   if (currentGame[0]) {
-     return currentGame[0].player.map((player, i) =>
-     <div key={i}>
-       <ListItem
-         primaryText={player}
-         rightAvatar={<Avatar src="https://storage.googleapis.com/material-icons/external-assets/v4/icons/svg/ic_face_black_24px.svg" />}
-       />
-     </div>
-   );
- }
- return null;
-}
-
-render () {
-  return (
-    <div style={containerStyle}>
-      <div style={{margin: 'auto',     width: 'inherit'}}>
-        <Card style={{margin: 'auto'}}>
-          <List style={{width:200}}>
-            <ListItem
-              primaryText="Code"
-              secondaryText={this.props.params.gameCode}
-              rightIconButton={rightIconMenu}
-              leftIcon={<ActionGrade color={pinkA200} />}
-            />
-          </List>
-          <Divider />
-          <List>
-            {this.renderPlayers.bind(this)()}
-          </List>
-        </Card>
-        <div style={{margin: 'auto' , display: 'flex'}}>
-          <RaisedButton
-
-            style={{margin: 'auto', display: 'flex', width: '100%'}}
-            label="Start Game"
-            primary={true}
+      if (currentGame[0]) {
+        return currentGame[0].player.map((player, i) =>
+        <div key={i}>
+          <ListItem
+            primaryText={player}
+            rightAvatar={<Avatar src="https://storage.googleapis.com/material-icons/external-assets/v4/icons/svg/ic_face_black_24px.svg" />}
           />
         </div>
+      );
+    }
+    return null;
+  }
+
+  render () {
+    return (
+      <div style={containerStyle}>
+        <div style={{margin: 'auto', width: 'inherit'}}>
+          <Card style={{margin: 'auto'}}>
+            <List>
+              <ListItem
+                primaryText="Code"
+                secondaryText={this.props.params.gameCode}
+                rightIconButton={rightIconMenu}
+                leftIcon={<ActionGrade color={pinkA200} />}
+              />
+            </List>
+            <Divider />
+            <List>
+              {this.renderPlayers.bind(this)()}
+            </List>
+          </Card>
+          <div style={{margin: 'auto' , display: 'flex'}}>
+            <RaisedButton
+
+              style={{margin: 'auto', display: 'flex', width: '100%'}}
+              label="Start Game"
+              primary={true}
+            />
+          </div>
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
 }
 
-   export default createContainer(() => {
-     return {
-       games: Games.find({}, { sort: { createdAt: -1 } }).fetch(),
-     };
-   }, Lobby);
+export default createContainer(() => {
+  return {
+    games: Games.find({}, { sort: { createdAt: -1 } }).fetch(),
+  };
+}, Lobby);
